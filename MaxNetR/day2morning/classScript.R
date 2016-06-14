99bd44f4bc1846f955bb9cde3a3f68808918a4b2
Tim Riffe <tim.riffe@gmail.com>
2016-06-14 12:01:20 +0200

    morning code day 2

--- MaxNetR/day2morning/classScript.R
+++ MaxNetR/day2morning/classScript.R
@@ -1,4 +1,4 @@
-
+# day 2 morning
 # Author: tim
 ###############################################################################
 
@@ -41,6 +41,12 @@
 # you'll need to change this to whatever your working folder
 # is for this session.
 setwd("/home/tim/git/MaxNetR/MaxNetR/day2morning")
+# you can also do it manually in the tabs above in Rstudio:
+# Session/set working directory
+
+
+# clear all objects in workspace:
+rm(list = ls())
 
 # this will load an object called HFD into our workspace
 load("HFDall.Rdata")
@@ -51,6 +57,8 @@ ls()
 str(HFD)
 head(HFD)
 dim(HFD)
+class(HFD)
+summary(HFD)
 
 # what countries do we have?
 unique(HFD$CNTRY)
@@ -65,9 +73,12 @@ library(reshape2)
 
 # acast() creates an array from a long piece of data. It uses a formula syntax:
 # (this avoids a loop)
-HFDarray <- acast(HFD, Age~Year~CNTRY, value.var = "ASFR")
+head(HFD)
+HFDarray    <- acast(HFD, Age ~ Year ~ CNTRY, value.var = "ASFR")
 dim(HFDarray)
 dimnames(HFDarray)
+HFDarray[ , , "TWN"]
+
 # so we have age by year by country. 
 # These are age-period matrices stacked by country
 
@@ -82,7 +93,9 @@ HFDarray[, "1950", "SWE"]
 
 # let's make a spaghetti plot
 ages <- 12:55
-matplot(ages,HFDarray[,,"SWE"],type = 'l')
+SWE <- HFDarray[,,"SWE"]
+dim(SWE)
+matplot(ages, HFDarray[,,"SWE"], type = 'l')
 
 # all gray and solid lines
 matplot(ages,HFDarray[,,"SWE"],
@@ -103,10 +116,11 @@ matplot(ages,HFDarray[,,"SWE"],
 
 years     <- as.integer(colnames(HFDarray))
 # how about we highlight every 5th year using some color gradient?
-
 yr5       <- years[years %% 5 == 0] # introducing modulo!
 # these are integers, but we need characters to select:
 yr5c      <- as.character(yr5)
+
+
 # Make a color ramp:
 library(RColorBrewer)
 display.brewer.all()
@@ -119,7 +133,10 @@ length(yr5) # uh oh, we need more blues!!
 # 1) give it a vector of colors
 # 2) tell it which color space to interpolate over:
 # 3) it spits back a ramp function for you
-myramp   <- colorRampPalette(mypalette, space = "Lab")
+
+#mypalette <- c("white","royalblue","blue")
+myramp    <- colorRampPalette(mypalette, space = "Lab")
+
 # it works like this: just ask for N colors 
 # and it will interpolate through the space you gave
 # and give you N colors in 'even' intervals.
@@ -161,7 +178,7 @@ rect(
 		par("usr")[3], 
 		par("usr")[2], 
 		par("usr")[4], 
-		col = gray(.92)) # ?rect
+		col = gray(.95)) #
 # now add the color lines
 matplot(ages,HFDarray[, yr5c,"SWE"],
 		type = 'l', 
@@ -196,37 +213,263 @@ matplot(ages,HFDarray[, ,"SWE"],
 		type = 'l', 
 		lty = 1, 
 		lwd = 2, # thicker lines
-		col = paste0(myramp(length(years)),60),
+		#col = myramp(length(years)),
+		 col = paste0(
+		   myramp(length(years)),
+		   60
+		              ),
 		add = TRUE) 
 # ahhhh now we see undulations!
 # locator(1)
-text(28, .13, "undulations?", col = "magenta", srt = -35, cex = 2)
+text(28, .13, "undulations?
+     maybe?", col = "magenta", srt = -35, cex = 2)
+# fromxy <- locator(1)
+# toxy   <- locator(1)
+# arrows(fromxy$x, fromxy$y, toxy$x, toxy$y)
+
 ################################################################
 # stuff to expand:
 
 # 1) TFR for each year and country, plot as lines.
 # (this avoids a loop)
-TFR <- apply(HFDarray, 3, colSums)
-matplot(years, TFR, type = 'l', lty = 1, col = "#00000050")
+TFR   <- apply(
+          HFDarray, 
+          3, 
+          colSums)
+
+TFR   <- apply(
+          HFDarray, 
+          c(2,3), 
+          sum)
+
+
+matplot(years, TFR, type = 'l', lty = 1, col = "#00000080")
 
 # 1.1) what is the average TFR per year in the HFD?
 # (this avoids a loop)
 lines(years, rowMeans(TFR, na.rm = TRUE), col = "red")
 
 # 1.2) what is the population-weighted average TFR per year?
-
+head(HFD)
 Bx <- acast(HFD, Age~Year, sum, value.var = "Births")
 Ex <- acast(HFD, Age~Year, sum, value.var = "Exposure")
+dim(Bx)
+dim(Ex)
+
 Fx <- Bx / Ex
 # OK, so bigger countries are having a bigger swing up?
 lines(years, colSums(Fx), col = "blue")
 
+ls()
+
 # 2) MAB for each year and country, plot as lines
+# cool, we can write a function:
+
+wmean <- function(x, w){
+	result <- sum(x * w) / sum(w)
+	return(result)
+}
+
+# 2.1) how does it work?
+# fertility rates are the weights
+# age is the thing being weighted
+wmean(x = 12:55 + .5, w = Fx[,"1891"])
+# MAB = mean age at childbearing in a mortality-free stationary population.
+
+# 2.2) so how about all MAB in the HFD?
+# here's an esoteric (one of many) R- way of doing this calc:
+MAB <- apply(HFDarray, c(2,3), wmean, x = 12.5:55.5)
+MAB
+
+# wmeanMat <- function(x, W){
+#   result <- colSums(x * W, na.rm = TRUE) / colSums(W, na.rm = TRUE)
+#   return(result)
+# }
+# apply(HFDarray, 3, wmean, x = 12.5:55.5)
+
+# but possibly the easier to read loop way of doing 
+# it would look like this:
+# 2.2.1) make a container:
+MAB2 <- MAB * NA
+# iterate over years (y takes steps)
+for (y in 1:length(years)){ 
+	# iterate over countries
+	for (cn in 1:dim(HFDarray)[3]){ 
+	  # (cn takes steps)
+		# since y and cn are counting integers, we can use
+		# them to index our results container.
+		# the same y and cn index to our HFD rates array.
+		MAB2[y, cn] <- wmean(x = 12.5:55.5, w = HFDarray[, y, cn])
+	}
+}
+#apply(HFDarray, c(2,3), wmean, x = 12.5:55.5)
+
+dim(TFR)
+dim(MAB)
+
+# 2.3) let's plot this against TFR:
+# 2.3.1) turn these matrices into vectors:
+tfr <- c(TFR)
+mab <- c(MAB)
+
+# what's the default scatter aesthetic?
+plot(tfr, mab)
+# how would we like to modify this?
+# do we want to animate it?
+plot(tfr, 
+     mab,
+     cex = .5)
+
+# choose solid points
+plot(tfr, 
+     mab,
+     cex = .5,
+     pch = 19)
+
+# 
+plot(tfr, 
+     mab,
+     cex = .5,
+     pch = 19,
+     col = "#00000050")
+
+# TFR, MAB
+
+plot(tfr, 
+     mab,
+     type = "n",
+     xlab = "TFR",
+     ylab = "MAB")
+countries <- dimnames(HFDarray)[[3]]
+for (cn in countries){
+  lines(TFR[,cn], 
+        MAB[,cn], 
+        col = "#0000A150", 
+        lwd = .7)
+}
+points(tfr, mab, 
+       cex = .5,
+       pch = 19,
+       col = "#00000050")
+lines(TFR[,"SWE"],MAB[,"SWE"],col = "red")
+lines(TFR[,"USA"],MAB[,"USA"],col = "blue")
+
+###############################################
+# I don't want to be responsible for neck pain:
+plot(tfr, 
+     mab,
+     cex = .5,
+     pch = 19,
+     col = "#00000050",
+     axes = FALSE,
+     xlab = "TFR",
+     ylab = "MAB")
+ylims <- range(mab, na.rm = TRUE)
+xlims <- range(tfr, na.rm = TRUE)
+# permission to draw outside of plotting area
+par(xpd = TRUE)
+# axis line
+segments(xlims[1]-.2, 
+         ylims[1], 
+         xlims[1]-.2, 
+         ylims[2])
+# artisanal ticks:
+yticks <- fivenum(mab)
+segments(xlims[1]-.2,
+         yticks,
+         xlims[1]-.25,
+         yticks)
+text(xlims[1]-.25, yticks, round(yticks,1), pos = 2)
+
+# again for x axis:
+xticks <- fivenum(tfr)
+# axis line
+segments(xlims[1], 
+         ylims[1] - .3, 
+         xlims[2], 
+         ylims[1]-.3)
+
+# tick marks
+segments(xticks,
+         ylims[1] - .3,
+         xticks,
+         ylims[1] - .5)
+text(xticks, ylims[1]-.5, round(xticks,1), pos = 1)
+# let's do this on the fly
+
+
+#####################################################
 
 # 3) look at surface: are those really undulations?
 #    Hypothesis: these are postponements, and each implies
 #    a temporary drop in period measures. If you look
 #    at cohort rates, they might not look like undulations at all
 
+SWE <- HFDarray[,,"SWE"]
 
+# 1) the most basic surface function is image().
+# it does simple rasters.
+image(SWE) # and it does an old-fashioned transpose
+
+# get centered ages
+ages.centered   <- ages + .5
+# get centered years:
+years.centered  <- years + .5
+# try again:
+image(years.centered, 
+      ages.centered, 
+	  t(SWE),
+	  xlim = range(years) + c(0,1),
+	  ylim = range(ages) + c(0,1),
+	  asp = 1)
+# it gets wide! But the colors aren't so great.
+# we could make a new ramp or recycle the old one:
+myramp(10)
+# there is a cool function called pretty() in R
+# that finds 'pretty' break points for you.
+
+pretty(SWE)
+pretty(SWE,12) # very useful.
+# we need 1 less color than there are breaks
+# so lets set some breaks:
+brks <- pretty(SWE,12)
+# and set some colors:
+cols <- myramp(length(brks) - 1)
+
+# retry:
+image(years.centered, 
+		ages.centered, 
+		t(SWE),
+		xlim = range(years) + c(0,1),
+		ylim = range(ages) + c(0,1),
+		asp = 1,
+		breaks = brks, # set breaks
+		col = cols)    # ans colors
+
+# but I don't want light blue where there are 0s:
+SWE[SWE == 0] <- NA
+image(years.centered, 
+	  ages.centered, 
+	  t(SWE),
+	  xlim = range(years) + c(0,1),
+	  ylim = range(ages) + c(0,1),
+      asp = 1,
+      breaks = brks, # set breaks and colors
+      col = cols,
+      xlab = "Year",
+      ylab = "Age")    
+
+# OK doesn't make a huge difference.
+# one could add a legend, but contours are a bit better methinks:
+contour(years.centered, 
+		ages.centered, 
+		t(SWE),
+		breaks = brks,
+		method="flattest",
+		col = "#00000080", # OK, but I wish the labels were brighter
+		add = TRUE) # this is key
+
+# Lexis grid lines?
+# there's actually a lot more one can do with this kind of plot.
+# requests?
 
